223c1282a28fc11838edb517812391ef42a93128
sarthikalathiya <sarthi.kalathiya@simformsolutions.com>
2025-01-22 14:48:58 +0530

    feat: added toggle theme

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-onkeydown = function(e) {
-    if (e.key == "Enter") {
-        console.log("Enter key pressed")
+document.querySelector('.theme-toggle').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+    const icon = document.querySelector('.theme-toggle i');
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
     }
-}
+});
+
+document.getElementById('trigToggle').addEventListener('click', function() {
+    document.getElementById('trigDropdown').classList.toggle('show');
+    document.querySelector('#trigToggle .fa-chevron-down').classList.toggle('rotate-arrow');
+});
