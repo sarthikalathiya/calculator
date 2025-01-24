09fdf9f221ea4a456a5a2dd8223c65a962b217df
sarthikalathiya <sarthi.kalathiya@simformsolutions.com>
2025-01-24 10:15:47 +0530

    feat: added toggle sign

--- js/Calculator.js
+++ js/Calculator.js
@@ -18,8 +18,38 @@ Calculator.prototype.updateDisplay = function() {
     }
 };
 
+Calculator.prototype.toggleSign = function() {
+    if (!this.displayValue) return;
+    
+    const lastNumber = this.displayValue.split(/[\+\-\*\/\(\)]/).pop();
+    if (!lastNumber) return;
+
+    const lastNumberIndex = this.displayValue.lastIndexOf(lastNumber);
+    
+    if (lastNumberIndex === -1) return;
+
+    const hasNegative = this.displayValue[lastNumberIndex - 1] === '-';
+    const hasPreviousOperator = lastNumberIndex > 0 && 
+                               MathUtils.isOperator(this.displayValue[lastNumberIndex - 2]);
+
+    if (hasNegative && (lastNumberIndex === 1 || hasPreviousOperator)) {
+        this.displayValue = this.displayValue.slice(0, lastNumberIndex - 1) + 
+                           this.displayValue.slice(lastNumberIndex);
+    } else if (lastNumberIndex === 0) {
+        this.displayValue = '-' + this.displayValue;
+    } else {
+        this.displayValue = this.displayValue.slice(0, lastNumberIndex) + 
+                          '-' + this.displayValue.slice(lastNumberIndex);
+    }
+    this.updateDisplay();
+};
+
 Calculator.prototype.appendNumber = function(number) {
     if (number === '.' && this.displayValue.includes('.')) return;
+    if (number === 'Plus/Minus') {
+        this.toggleSign();
+        return;
+    }
     this.displayValue += number;
     this.updateDisplay();
 };
